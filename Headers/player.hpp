bd902a3c4e939073299207ff424c554cc1223a93
A-Wodnicki <wodnicki.adrian4@gmail.com>
2023-06-01 22:54:49 +0200

    Super shoot feature

--- Headers/player.hpp
+++ Headers/player.hpp
@@ -12,18 +12,26 @@ class Player : public Ship {
 
   void update(const float& deltaTime);
   int getHp() const;
+  int getPowerUpCount() const;
+
+  void addHp();
+  void addPowerUp();
 
  private:
   void handleControl(const float& deltaTime);
-  void shoot(int angle);
-  void shootNarrow();
-  void shootWide();
+  void shoot(int angle, int projectilesToShoot = 4);
+  void shootSuper();
 
   sf::Keyboard::Key shootButton1 = sf::Keyboard::Key::J;
   sf::Keyboard::Key shootButton2 = sf::Keyboard::Key::K;
   const sf::Texture& projectile;
   sf::Clock projectileCooldown;
+  int projectileAngle = 10;
   int hp;
+  int powerUpCount;
+  sf::Clock superDuration;
+  sf::Clock superCooldown;
+  bool shootingSuper;
 };
 
 #endif  // PLAYER_HPP
--- Sources/player.cpp
+++ Sources/player.cpp
@@ -12,15 +12,33 @@ Player::Player(const sf::Texture& texture,
            300,
            projectiles),
       projectile(projectile),
-      hp(3) {}
+      hp(3),
+      powerUpCount(3),
+      shootingSuper(false) {}
 
 void Player::update(const float& deltaTime) {
-  handleControl(deltaTime);
   Ship::update(deltaTime);
+
+  if (shootingSuper)
+    shootSuper();
+
+  handleControl(deltaTime);
 }
 
 int Player::getHp() const {
   return hp;
+}
+
+int Player::getPowerUpCount() const {
+  return powerUpCount;
+}
+
+void Player::addHp() {
+  hp++;
+}
+
+void Player::addPowerUp() {
+  powerUpCount++;
 }
 
 void Player::handleControl(const float& deltaTime) {
@@ -53,6 +71,13 @@ void Player::handleControl(const float& deltaTime) {
     changeAnimationType();
   }
 
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::L) && powerUpCount &&
+      !shootingSuper && superCooldown.getElapsedTime().asSeconds() > 3) {
+    powerUpCount--;
+    shootingSuper = true;
+    superDuration.restart();
+  }
+
   if (sf::Keyboard::isKeyPressed(shootButton1) &&
       projectileCooldown.getElapsedTime().asSeconds() >= 0.075) {
     shoot(0);
@@ -60,19 +85,39 @@ void Player::handleControl(const float& deltaTime) {
 
   if (sf::Keyboard::isKeyPressed(shootButton2) &&
       projectileCooldown.getElapsedTime().asSeconds() >= 0.075) {
-    shoot(20);
+    shoot(projectileAngle);
+    projectileAngle += 2;
+    if (projectileAngle >= 40)
+      projectileAngle = 10;
   }
 
   if (projectileCooldown.getElapsedTime().asSeconds() >= 0.075)
     projectileCooldown.restart();
 }
 
-void Player::shoot(int angle) {
-  for (int i = 0; i < 4; ++i)
+void Player::shoot(int angle, int projectilesToShoot) {
+  for (int i = 0; i < projectilesToShoot; ++i)
     projectiles.emplace_back(std::make_unique<Projectile>(
         projectile, 1000,
-        !angle ? 0 : 360 - angle % 90 + 2 * i * (360 - (360 - angle % 90)) / 3,
+        !angle
+            ? 0
+            : 360 - angle % 90 +
+                  2 * i * (360 - (360 - angle % 90)) / (projectilesToShoot - 1),
         true,
-        sf::Vector2f(getGlobalBounds().left + getGlobalBounds().width / 3 * i,
+        sf::Vector2f(getGlobalBounds().left +
+                         getGlobalBounds().width / (projectilesToShoot - 1) * i,
                      getGlobalBounds().top)));
+}
+
+void Player::shootSuper() {
+  if (superDuration.getElapsedTime().asSeconds() >= 1) {
+    shootingSuper = false;
+    superCooldown.restart();
+  }
+
+  if (projectileCooldown.getElapsedTime().asSeconds() < 0.075)
+    return;
+
+  shoot(80, 40);
+  projectileCooldown.restart();
 }
